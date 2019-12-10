7b871e8ca942d07b143be613518557960822f136
Jeremy Greer <jex.grizzle@gmail.com>
2019-12-09 21:46:01 -0800

    Use helper to generate list of nav links

--- lib/journal_web/views/layout_view.ex
+++ lib/journal_web/views/layout_view.ex
@@ -1,3 +1,20 @@
 defmodule JournalWeb.LayoutView do
   use JournalWeb, :view
+
+  def get_nav(conn) do
+    [
+      %{
+        :label => "Home",
+        :href => Routes.page_path(conn, :index)
+      },
+      %{
+        :label => "Entries",
+        :href => Routes.entry_path(conn, :index),
+      },
+      %{
+        :label => "Changes",
+        :href => Routes.change_path(conn, :index),
+      },
+    ]
+  end
 end
