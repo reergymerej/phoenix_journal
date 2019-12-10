5a19509033823df1d6a132d44a1f49d35685cf4d
Jeremy Greer <jex.grizzle@gmail.com>
2019-12-10 15:50:19 -0800

    Show title for entries listing

--- lib/journal_web/views/entry_view.ex
+++ lib/journal_web/views/entry_view.ex
@@ -1,3 +1,12 @@
 defmodule JournalWeb.EntryView do
   use JournalWeb, :view
+
+  def get_title(%{title: title, text: text}) do
+    case title do
+      nil
+        -> text
+      _
+        -> title
+    end
+  end
 end
